fcb468466a0ebd986cd7b2623571f402e3fb1234
Nekstoreo450 <ngutierrezarias450@gmail.com>
2022-04-18 11:34:18 -0500

    avance calcauladora v2 #1

--- calculadora_v2.java
+++ calculadora_v2.java
@@ -8,8 +8,8 @@ public class calculadora_v2 {
 
     public static void main(String[] args) {
         
-        Scanner opcion_menu = new Scanner(System.in);
-
+        Scanner sc = new Scanner(System.in);
+        int opcion_menu = 0;
         System.out.println("                ___   _   _    ___ _   _ _      _   ___   ___  ___    _     ___ ___  ___   __               ");
         System.out.println("       / __| /_\\ | |  / __| | | | |    /_\\ |   \\ / _ \\| _ \\  /_\\   | _ \\ _ \\/ _ \\  \\ \\ / /_  )   ");
         System.out.println("      | (__ / _ \\| |_| (__| |_| | |__ / _ \\| |) | (_) |   / / _ \\  |  _/   / (_) |  \\ V / / /           ");
@@ -25,6 +25,59 @@ public class calculadora_v2 {
         System.out.println("**                                                                                                   **");
         System.out.println("*******************************************************************************************************");
         System.out.println("*******************************************************************************************************");
+        System.out.println("");
+        System.out.println("Ingresa el valor de tu opcion: ");
+        opcion_menu = sc.nextInt();
+
+        switch (opcion_menu) {
+            case 1:
+            
+            int fecha_zodiaco[] = new int [2]; 
+
+            System.out.println("*******************************************************************************************************");
+            System.out.println("**                Muy bien, Seleccionaste Calcule el signo del zodiaco de una persona.               **");
+            System.out.println("*******************************************************************************************************");
+            System.out.println("*******************************************************************************************************");
+            System.out.println("**                       Ahora ingresa el mes de tu nacimiento                                       **");
+            System.out.println("*******************************************************************************************************");
+            fecha_zodiaco[0] = sc.nextInt();
+
+            System.out.println("*******************************************************************************************************");
+            System.out.println("**                         Ahora ingresa el dia de tu nacimiento                                     **");
+            System.out.println("*******************************************************************************************************");
+            fecha_zodiaco[1] = sc.nextInt();
+
+            if (fecha_zodiaco[0] >= 1 && fecha_zodiaco[0] <= 2 && fecha_zodiaco[1] >= 21 && fecha_zodiaco[1] <= 19) {
+                System.out.println("*******************************************************************************************************");
+                System.out.println("**                             Tu Signo Zodiacal es Acuario                                          **");
+                System.out.println("*******************************************************************************************************");
+                }
+            } else if (fecha_zodiaco[0] >= 1 && fecha_zodiaco[0] <= 2 && fecha_zodiaco[1] >= 21 && fecha_zodiaco[1] <= 19){
+                System.out.println("*******************************************************************************************************");
+                System.out.println("**                             Tu Signo Zodiacal es Piscis                                          **");
+                System.out.println("*******************************************************************************************************");
+            }
+
+                break;
+            case 2:
+                
+                break;
+            
+            case 3:
+                
+                break;
+            
+            case 4:
+                
+                break;
+            
+            case 5:
+                
+                break;
+
+            default:
+                break;
+        }
 
     }
 }
