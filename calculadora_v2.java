681039e807235612024b3f6dc89f76e317200587
Nekstoreo450 <ngutierrezarias450@gmail.com>
2022-04-18 10:55:44 -0500

    Nueva Calculadora

---
+++ calculadora_v2.java
@@ -0,0 +1,30 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/**
+ * calculadora_v2
+ */
+public class calculadora_v2 {
+
+    public static void main(String[] args) {
+        
+        Scanner opcion_menu = new Scanner(System.in);
+
+        System.out.println("                ___   _   _    ___ _   _ _      _   ___   ___  ___    _     ___ ___  ___   __               ");
+        System.out.println("       / __| /_\\ | |  / __| | | | |    /_\\ |   \\ / _ \\| _ \\  /_\\   | _ \\ _ \\/ _ \\  \\ \\ / /_  )   ");
+        System.out.println("      | (__ / _ \\| |_| (__| |_| | |__ / _ \\| |) | (_) |   / / _ \\  |  _/   / (_) |  \\ V / / /           ");
+        System.out.println("       \\___/_/ \\_\\____\\___|\\___/|____/_/ \\_\\___/ \\___/|_|_\\/_/ \\_\\ |_| |_|_\\\\___/    \\_/ /___|");
+        System.out.println("*******************************************************************************************************");
+        System.out.println("*******************************************************************************************************");
+        System.out.println("**                                                                                                   **");
+        System.out.println("**                         1. Calcule el signo del zodiaco de una persona.                           **");
+        System.out.println("**                         2. Calcule la cantidad de dias vividos por una personas.                  **");
+        System.out.println("**                         3. Calcular la edad de una persona en años de perro.                     **");
+        System.out.println("**                         4. Calcular el numero según la numerologia.                              **");
+        System.out.println("**                         5. Hacer una donacion a los Creadores.                                    **");
+        System.out.println("**                                                                                                   **");
+        System.out.println("*******************************************************************************************************");
+        System.out.println("*******************************************************************************************************");
+
+    }
+}
