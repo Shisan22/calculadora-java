bd0382d4516e83c41529ba6fb0f1a3182bf40927
esteban <estebanquiceno2004@email.com>
2022-04-17 19:02:38 -0500

    inicio zodiaco

--- enero.java
+++ enero.java
@@ -1,17 +1,19 @@
-enero
-febreo
-marzo
-abril
-mayo
-junio
-julio
-agosto
-septiembre
-octubre
-noviembre
-diciembre
-x (años);
-edad perro = (años*7);
-if (m = true){
-    
-}
+ public class principal {
+        public static void main(string[] args) {
+            Scanner Zodiaco=new Scanner(System.in);
+            int Dia,Mes;
+            System.out.println("ingrese el mes de nacimiento:"
+                + "\n1-Enero"
+                + "\n2-Febrero"
+                + "\n3-Marzo"
+                + "\n4-Abril"
+                + "\n5-Mayo"
+                + "\n6-Junio"
+                + "\n7-Julio"
+                + "\n8-Agosto"
+                + "\n9-Septiembre"
+                + "\n10-Octubre"
+                + "\n11-Noviembre"
+                + "\n12-Diciembre");
+        }
+ }
