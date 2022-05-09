5457d6c8a397ff8cd333bad8343c6e3ec1b187d2
esteban <estebanquiceno2004@email.com>
2022-05-09 13:09:22 -0500

    v 0.2

--- InterfasGrafica.java
+++ InterfasGrafica.java
@@ -4,8 +4,10 @@ import java.awt.event.*;
 public class InterfasGrafica extends JFrame implements ActionListener{
 
   private JLabel label1;
+  private JLabel label2;  
   private JTextField respuesta;
   private JButton botonAceptar;
+  private JButton botoncerrar;
   
   public InterfasGrafica(){
     setLayout(null);
@@ -13,6 +15,10 @@ public class InterfasGrafica extends JFrame implements ActionListener{
     label1.setBounds(10,20,300,30);
     add(label1);
 
+    label2 = new JLabel("ingrese su mes");
+    label2.setBounds(10,50,100,30);
+    add(label2);
+    
     respuesta = new JTextField();
     respuesta.setBounds(120,180,150,20);
     add(respuesta);
@@ -21,12 +27,20 @@ public class InterfasGrafica extends JFrame implements ActionListener{
     botonAceptar.setBounds(10,210,100,30);
     add(botonAceptar);
     botonAceptar.addActionListener(this);
+    
+    botoncerrar = new JButton("cerrar");
+    botoncerrar.setBounds(300,270,100,30);
+    add(botoncerrar);
+    botoncerrar.addActionListener(this);
   }
 
   public void actionPerformed(ActionEvent e){
-  if(e.getSource() == botonAceptar){
-    String texto = respuesta.getText();
-    setTitle(texto);
+    if(e.getSource() == botonAceptar){
+      String texto = respuesta.getText();
+      setTitle(texto);
+      }
+    if(e.getSource() == botoncerrar){
+      System.exit(0);
     }
   }
   
